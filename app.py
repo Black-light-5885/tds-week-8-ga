a3b3007251cc7879f90373d6928efd18c8810afa
Bala Murugan <99783065+Black-light-5885@users.noreply.github.com>
2023-04-18 11:53:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.write("Welcome !")
 
-st.title("TDS week 8 assignemnt")
+st.title("TDS Week-8 Assignment")
 st.write("---")
 st.header("Find the largest among the 3 given numbers")
 with st.form(key="form1"):
@@ -12,5 +12,5 @@ with st.form(key="form1"):
     button = st.form_submit_button("Find the maximum")
 if button:
     st.success(f"The maximum number is {max([n1,n2,n3])} ")
-    if max([n1,n2,n3])==0:
-        st.write("Instead of default put some random values!")
+    if n1==0 and n2 == 0 and n3 ==0:
+        st.write("Instead of default, Try with some random values!")
