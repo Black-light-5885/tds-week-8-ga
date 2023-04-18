fb01b81037111768e6c74da4b7edb6935dd0e56b
Bala Murugan <99783065+Black-light-5885@users.noreply.github.com>
2023-04-18 11:29:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ st.header("Finding the maximum number given in the three numbers.")
 n1 = st.number_input("First Number :")
 n2 =st.number_input("Second Number :")
 n3 =st.number_input("Third Number :")
-
-st.write("The maximum number is ",max([n1,n2,n3]))
+m = max([n1,n2,n3])
+st.write(str(m))
+st.write("The maximum number is ")
