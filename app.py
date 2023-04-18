968553486e58abd693a382cdfadee343c4feb1a0
Bala Murugan <99783065+Black-light-5885@users.noreply.github.com>
2023-04-18 11:45:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ st.title("TDS week 8 assignemnt")
 st.write("---")
 st.header("Find the largest among the 3 given numbers")
 with st.form(key="form1"):
-    n1 = st.number_input("Selct Your First Number :")
-    n2 =st.number_input("Select Your Second Number :")
-    n3 =st.number_input("Select your Third Number :")
+    n1 = st.number_input("Enter the First Number :")
+    n2 =st.number_input("Enter the Second Number :")
+    n3 =st.number_input("Enter the Third Number :")
     button = st.form_submit_button("Find the maximum")
 if button:
     st.success(f"The maximum number is {max([n1,n2,n3])} ")
